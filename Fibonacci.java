806964a1dd205b0f98a72923143749d04b20409d
JhonPrFer <62401441+JhonPrFer@users.noreply.github.com>
2021-09-03 22:37:14 -0300

    colocando espaçamento

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,7 +7,7 @@ public class Fibonacci {
 		int num2 = 1;
 		while(num1 <= 100 && num2 <=100 ) {
 			System.out.print(num1 + " ");
-			System.out.print(num2+" " );
+			System.out.print(num2 + " " );
 			num1+=num2;
 			num2+=num1;
 		}

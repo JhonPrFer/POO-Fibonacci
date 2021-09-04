c10c00aceb8b45cbcce7b5fa208115b8e8de556d
JhonPrFer <62401441+JhonPrFer@users.noreply.github.com>
2021-09-03 22:36:41 -0300

    Criando calsse Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+
+public class Fibonacci {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 0;
+		int num2 = 1;
+		while(num1 <= 100 && num2 <=100 ) {
+			System.out.print(num1 + " ");
+			System.out.print(num2+" " );
+			num1+=num2;
+			num2+=num1;
+		}
+    
+	}
+
+}
